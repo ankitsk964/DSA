50d6d76576cb57c84001ba775207a67c42455356
Ankit Kumar <gamingwithankit964@gmail.com>
2023-10-02 12:47:10 +0530

    Hollow inverted  half pyramid

---
+++ Hollow inverted half pyramid.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Ankit Kumar on 10/2/2023.
+//
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    for(int i = 1; i<=n ; i++){
+
+        for(int j = 1; j<=(n-i+1) ; j++){
+            if(i==1 || i==n || j==1 || j==(n-i+1)){
+                cout << "*";
+            }
+            else{
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+
+}
