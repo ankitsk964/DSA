056d890064f41fa38569e1ed9b2a8e6151a733fc
Ankit Kumar <gamingwithankit964@gmail.com>
2023-10-05 18:33:25 +0530

    very easy, took only 2 minutes

---
+++ Floyd's triangle pattern.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Ankit Kumar on 10/5/2023.
+//
+#include <iostream>
+using namespace std;
+int main(){
+
+    int n,count = 1;
+    cin >> n;
+
+    for(int i = 1; i<= n; i++){
+        for(int j = 1; j<=i ; j++){
+            cout << count << " ";
+            count++;
+        }
+        cout <<endl;
+    }
+    return 0;
+}
